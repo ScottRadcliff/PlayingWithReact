25677fa798895bf17faf9cfb9ceefec695f7465d
Scott Radcliff <radcliffsc@gmail.com>
2015-03-20 12:43:54 -0400

    WIP

---
+++ app.rb
@@ -0,0 +1,18 @@
+require "sinatra"
+require "sinatra/json"
+require 'sinatra/cross_origin'
+
+configure do
+  enable :cross_origin
+end
+
+
+get "/data.json" do
+  data = {
+    lastWeekPercent: 10,
+    thisWeekPercent: 3,
+    averagePercent: 10,
+    completePercent: 3,
+    onTimePercent: 12 }
+  json data
+end
